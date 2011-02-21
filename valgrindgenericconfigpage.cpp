45d142e22266cec8c312a39b8ac65e680bd1fc3c
mathieulornac <mathieu.lornac@gmail.com>
2011-02-21 22:35:17 +0000

    valgrindgenericconfigpage reindentation

--- valgrindgenericconfigpage.cpp
+++ valgrindgenericconfigpage.cpp
@@ -22,71 +22,70 @@
 
 #include "ui_valgrindgenericconfig.h"
 
-
 ValgrindGenericConfigPage::ValgrindGenericConfigPage(QWidget *parent)
-  : LaunchConfigurationPage(parent)
+    : LaunchConfigurationPage(parent)
 {
-  ui = new Ui::ValgrindGenericConfig();
-  ui->setupUi(this);
+    ui = new Ui::ValgrindGenericConfig();
+    ui->setupUi(this);
 
-  connect( ui->valgrindExecutable, SIGNAL(textChanged(QString)), SIGNAL(changed()) );
-  connect( ui->valgrindExecutable, SIGNAL(urlSelected(KUrl)), SIGNAL(changed()) );
-  connect( ui->valgrindParameters, SIGNAL(textEdited(QString)), SIGNAL(changed()) );
-  connect( ui->limitErrors, SIGNAL(toggled(bool)), SIGNAL(changed()) );
-  connect( ui->maxStackSize, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
-  connect( ui->stackDepth, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
+    connect( ui->valgrindExecutable, SIGNAL(textChanged(QString)), SIGNAL(changed()) );
+    connect( ui->valgrindExecutable, SIGNAL(urlSelected(KUrl)), SIGNAL(changed()) );
+    connect( ui->valgrindParameters, SIGNAL(textEdited(QString)), SIGNAL(changed()) );
+    connect( ui->limitErrors, SIGNAL(toggled(bool)), SIGNAL(changed()) );
+    connect( ui->maxStackSize, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
+    connect( ui->stackDepth, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
 
-  QStringList	tools;
+    QStringList	tools;
 
-  tools << "memcheck";
-  tools << "cachegrind";
-  tools << "callgrind";
-  tools << "helgrind";
-  tools << "massif";
+    tools << "memcheck";
+    tools << "cachegrind";
+    tools << "callgrind";
+    tools << "helgrind";
+    tools << "massif";
 
-  ui->currentTool->addItems(tools);
+    ui->currentTool->addItems(tools);
 
-  connect( ui->currentTool, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()) );
+    connect( ui->currentTool, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()) );
 }
 
 KIcon ValgrindGenericConfigPage::icon() const
 {
-  return KIcon("fork");
+    return KIcon("fork");
 }
 
 void ValgrindGenericConfigPage::loadFromConfiguration(const KConfigGroup &cfg, KDevelop::IProject *)
 {
-  bool wasBlocked = signalsBlocked();
-  blockSignals(true);
+    bool wasBlocked = signalsBlocked();
+    blockSignals(true);
 
-  ui->valgrindExecutable->setUrl( cfg.readEntry( "Valgrind Executable", KUrl( "/usr/bin/valgrind" ) ) );
-  ui->valgrindParameters->setText( cfg.readEntry( "Valgrind Arguments", "" ) );
-  ui->stackDepth->setValue( cfg.readEntry( "Framestack Depth", 12 ) );
-  ui->maxStackSize->setValue( cfg.readEntry( "Maximum Framestack Size", 2000000 ) );
-  ui->limitErrors->setChecked( cfg.readEntry( "Limit Errors", false ) );
+    ui->valgrindExecutable->setUrl( cfg.readEntry( "Valgrind Executable", KUrl( "/usr/bin/valgrind" ) ) );
+    ui->valgrindParameters->setText( cfg.readEntry( "Valgrind Arguments", "" ) );
+    ui->stackDepth->setValue( cfg.readEntry( "Framestack Depth", 12 ) );
+    ui->maxStackSize->setValue( cfg.readEntry( "Maximum Framestack Size", 2000000 ) );
+    ui->limitErrors->setChecked( cfg.readEntry( "Limit Errors", false ) );
 
-  int	tool_index;
-  QString	toolname;
-  toolname = cfg.readEntry( "Current Tool", "memcheck" );
-  tool_index = ui->currentTool->findText(toolname);
-  ui->currentTool->setCurrentIndex(tool_index);
+    int	tool_index;
+    QString	toolname;
+    toolname = cfg.readEntry( "Current Tool", "memcheck" );
+    tool_index = ui->currentTool->findText(toolname);
+    ui->currentTool->setCurrentIndex(tool_index);
 
-  blockSignals(wasBlocked);
+    blockSignals(wasBlocked);
 }
 
 void ValgrindGenericConfigPage::saveToConfiguration(KConfigGroup cfg, KDevelop::IProject *) const
 {
-  cfg.writeEntry( "Valgrind Executable", ui->valgrindExecutable->url() );
-  cfg.writeEntry( "Valgrind Arguments", ui->valgrindParameters->text() );
-  cfg.writeEntry( "Framestack Depth", ui->stackDepth->value() );
-  cfg.writeEntry( "Maximum Framestack Size", ui->maxStackSize->value() );
-  cfg.writeEntry( "Limit Errors", ui->limitErrors->isChecked() );
-  cfg.writeEntry( "Current Tool", ui->currentTool->currentText() );
+    cfg.writeEntry( "Valgrind Executable", ui->valgrindExecutable->url() );
+    cfg.writeEntry( "Valgrind Arguments", ui->valgrindParameters->text() );
+    cfg.writeEntry( "Framestack Depth", ui->stackDepth->value() );
+    cfg.writeEntry( "Maximum Framestack Size", ui->maxStackSize->value() );
+    cfg.writeEntry( "Limit Errors", ui->limitErrors->isChecked() );
+    cfg.writeEntry( "Current Tool", ui->currentTool->currentText() );
 }
 
 QString	ValgrindGenericConfigPage::title() const
 {
-  return i18n("Global settings");
+    return i18n("Global settings");
 }
 
 // The factory
@@ -98,5 +97,5 @@ ValgrindGenericConfigPageFactory::~ValgrindGenericConfigPageFactory()
 
 KDevelop::LaunchConfigurationPage* ValgrindGenericConfigPageFactory::createWidget(QWidget * parent)
 {
-  return new ValgrindGenericConfigPage(parent);
+    return new ValgrindGenericConfigPage(parent);
 }
